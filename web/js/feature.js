28a8192ed03ff030519bb1ed3c8ab36a55a0be56
forestlet <hyf2021@outlook.com>
2021-05-25 10:19:04 +0800

    refactor & add language support

--- web/js/feature.js
+++ web/js/feature.js
@@ -6,41 +6,64 @@ const lang = navigator.language
 
 initAlbums()
 
-// init datepicker
-$('#datepicker').datepicker({
-    language: lang,
-    format: "yyyy-mm-dd",
-    maxViewMode: 2,
-    todayHighlight: true,
-    todayBtn: "linked",
-    autoclose: true
-});
-
-// init tooltip
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
-// init toast
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl)
-})
-
-album_cover_carousel.addEventListener('slide.bs.carousel', function (e) {
-    current = e.to;
-    changeAlbumInfo()
-})
-
-$.getJSON("../asset/album.json", (data) => {
-    albums = data
-}).then(() => {
-    let date = getQueryString("date")
-    if (date != null) {
-        searchAlbums(date)
-        $('#datepicker').val(date)
+/* 
+ * after DOM load
+ */
+$(() => {
+    // add language
+    if (lang != "en") {
+        // hack:
+        $.get(`https://cdn.bootcdn.net/ajax/libs/bootstrap-datepicker/1.9.0/locales/bootstrap-datepicker.${lang}.min.js`, (data) => {
+            eval(data)
+        })
     }
+    $.getJSON(`./language/${lang}.json`, (data) => {
+        $("#share").eq(0).attr("title", data["share"])
+        $("#github").eq(0).attr("title", data["github"])
+        $("#search").eq(0).attr("title", data["search"])
+        $("#datepicker").eq(0).attr("placeholder", data["datepicker"])
+
+        $("title").eq(0).text(data["title"])
+        $("#album_artist_badge").eq(0).text(data["album_artist_badge"])
+        $("#album_release_time_badge").eq(0).text(data["album_release_time_badge"])
+        $("#no_album").eq(0).text(data["no_album"])
+        $("#footer_text").eq(0).text(data["footer_text"])
+        $("#submit").eq(0).text(data["submit"])
+        $("#copied").eq(0).text(data["copied"])
+    }).then(() => {
+        // init tooltip
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+
+        // init toast
+        var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+        var toastList = toastElList.map(function (toastEl) {
+            return new bootstrap.Toast(toastEl)
+        })
+
+        // init datepicker
+        $('#datepicker').datepicker({
+            language: lang,
+            format: "yyyy-mm-dd",
+            maxViewMode: 2,
+            todayHighlight: true,
+            todayBtn: "linked",
+            autoclose: true
+        })
+
+        // get data
+        $.getJSON("../asset/album.json", (data) => {
+            albums = data
+        }).then(() => {
+            let date = getQueryString("date")
+            if (date != null) {
+                searchAlbums(date)
+                $('#datepicker').val(date)
+            }
+        })
+    })
 })
 
 // add fn format
@@ -53,28 +76,49 @@ Date.prototype.format = function (fmt) {
         "s+": this.getSeconds(),
         "q+": Math.floor((this.getMonth() + 3) / 3),
         "S": this.getMilliseconds()
-    };
+    }
     if (/(y+)/.test(fmt)) {
-        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length))
     }
     for (var k in o) {
         if (new RegExp("(" + k + ")").test(fmt)) {
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)))
         }
     }
-    return fmt;
+    return fmt
 }
 
+/*
+ * events
+ */
 $("#search").click(() => {
     initAlbums()
 
     if (!$('#datepicker').val()) return
 
-    let pick_date = new Date($('#datepicker').val()).format("yyyy-MM-dd");
+    let pick_date = new Date($('#datepicker').val()).format("yyyy-MM-dd")
 
     searchAlbums(pick_date)
 })
 
+$("#share").click(() => {
+    $("#url").text(window.location.href)
+    let url = document.getElementById("url")
+    url.select()
+    document.execCommand("Copy")
+    $('.toast').toast('show')
+})
+
+album_cover_carousel.addEventListener('slide.bs.carousel', function (e) {
+    current = e.to
+    if (current == -1) return
+    changeAlbumInfo()
+})
+
+
+/* 
+ * functions
+ */
 function searchAlbums(pick_date) {
     history.replaceState(null, null, window.location.origin + window.location.pathname + "?date=" + pick_date)
 
@@ -82,7 +126,7 @@ function searchAlbums(pick_date) {
         if (album.release_time.replace(/\d\d\d\d-/, "") == pick_date.replace(/\d\d\d\d-/, "")) {
             date_albums.push(album)
         }
-    });
+    })
 
     if (date_albums.length == 0) {
         $("#no_album").show()
@@ -136,20 +180,11 @@ function singleAlbum() {
     $(".carousel-control-next").hide()
 }
 
-// 获得 url 参数
 function getQueryString(name) {
-    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    let r = window.location.search.substr(1).match(reg);
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
+    let r = window.location.search.substr(1).match(reg)
     if (r != null) {
-        return decodeURIComponent(r[2]);
-    };
-    return null;
+        return decodeURIComponent(r[2])
+    }
+    return null
 }
-
-$("#share").click(() => {
-    $("#url").text(window.location.href)
-    let url = document.getElementById("url")
-    url.select();
-    document.execCommand("Copy");
-    $('.toast').toast('show');
-})
